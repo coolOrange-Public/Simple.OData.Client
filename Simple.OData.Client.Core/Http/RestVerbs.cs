140a707528090ac838853537b4da044ad421e873
coolOrange <git@coolorange.com>
2016-06-24 15:09:54 +0200

    Changed RestVerbs.Patch in order to use common ODATA 'MERGE' for the requests

--- Simple.OData.Client.Core/Http/RestVerbs.cs
+++ Simple.OData.Client.Core/Http/RestVerbs.cs
@@ -1,14 +1,14 @@
 ﻿
 namespace Simple.OData.Client
 {
-    internal static class RestVerbs
-    {
-        // ReSharper disable InconsistentNaming
-        public const string Get = "GET";
-        public const string Post = "POST";
-        public const string Put = "PUT";
-        public const string Patch = "PATCH";
-        public const string Delete = "DELETE";
-        // ReSharper restore InconsistentNaming
-    }
+	internal static class RestVerbs
+	{
+		// ReSharper disable InconsistentNaming
+		public const string Get = "GET";
+		public const string Post = "POST";
+		public const string Put = "PUT";
+		public const string Patch = "MERGE";
+		public const string Delete = "DELETE";
+		// ReSharper restore InconsistentNaming
+	}
 }
